ca87ffc1417fcd02c7c4afdec8f5f725f6ce4f4d
Mrbysco <Mrbysco@users.noreply.github.com>
2021-05-30 19:13:19 +0200

    Fix knowledge being lost upon respawning

--- src/main/java/com/mrbysco/spelled/handler/CapabilityHandler.java
+++ src/main/java/com/mrbysco/spelled/handler/CapabilityHandler.java
@@ -2,11 +2,15 @@ package com.mrbysco.spelled.handler;
 
 import com.mrbysco.spelled.Reference;
 import com.mrbysco.spelled.api.SpelledAPI;
+import com.mrbysco.spelled.api.capability.ISpellData;
 import com.mrbysco.spelled.api.capability.SpelledCapProvider;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.nbt.INBT;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -25,5 +29,23 @@ public class CapabilityHandler {
         if(!player.world.isRemote) {
             SpelledAPI.syncCap((ServerPlayerEntity) player);
         }
+    }
+
+
+    @SubscribeEvent
+    public void onDeath(PlayerEvent.Clone event) {
+        // If not dead, player is returning from the End
+        if (!event.isWasDeath()) return;
+
+        PlayerEntity original = event.getOriginal();
+        PlayerEntity clone = event.getPlayer();
+
+        final Capability<ISpellData> capability = SpelledAPI.SPELL_DATA_CAP;
+        original.getCapability(capability).ifPresent(dataOriginal ->
+            clone.getCapability(capability).ifPresent(dataClone -> {
+                INBT nbt = capability.getStorage().writeNBT(capability, dataOriginal, null);
+                capability.getStorage().readNBT(capability, dataClone, null, nbt);
+            })
+        );
     }
 }
